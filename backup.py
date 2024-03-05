b78ac6fc5b9abd024870b53673c137c1bff7349a
Roman <roma564888@yandex.ru>
2024-03-05 16:15:03 +0300

    Fixed datime and added tcgi_archive backup

--- backup.py
+++ backup.py
@@ -1,89 +1,85 @@
-import yaml
+import shutil
+import sqlite3
 import logging
-import typing as tp
 import os
-from schemas import Config
-from pathlib import Path
-import time
-import datetime
 import re
+from modules.os_work import delete_files, get_current_date, get_current_time, archive_backup, load_yaml, move_file
+from pathlib import Path
+from modules import Config, Database
 
 
-def delete_files(path, days, regex):
-    current_time = time.time()
-    expiration_time = days * 24 * 60 * 60
+def create_backup(database: Database):
+    logging.info(f"Backup of {str(database.get_name)} started")
+    logging.info("Deleting old backups and archives")
 
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if re.search(regex, file) and (current_time - os.path.getmtime(file_path)) > expiration_time:
-                os.remove(file_path)
-                logging.info(f"Deleted {file_path}")
+    delete_files(database.get_backup_dir, database.get_delete_after_days, fr'^{str(database.get_name)}_backup_.*\.db$')
+    delete_files(database.get_backup_dir, database.get_delete_after_days, fr'^{str(database.get_name)}_backup_.*\.7z$')
+    if not re.match(fr'^tcgi.*_archive$', str(database.get_name), re.IGNORECASE):
+        delete_files(database.get_backup_dir, database.get_delete_after_days, fr'^{str(database.get_name)}_archive_.*\.db$')
 
+    logging.info("Old backups and archives are deleted")
 
-def start_services(services, sch_tasks, iis):
-    for serv in services:
-        logging.info("Turning on \"" + serv + "\" service")
-        logging.info("sc start \"" + serv + "\"")
-        os.system("sc start \"" + serv + "\"")
+    db_size = os.path.getsize(database.get_path)
+    free_space = shutil.disk_usage(database.get_backup_dir).free
 
-    for task in sch_tasks:
-        logging.info("Turning on \"" + task + "\" task")
-        logging.info("schtasks /change /tn \" " + task + "\" /enable")
-        os.system("schtasks /change /tn \" " + task + "\" /enable")
+    gb = 1073741824
 
-    if iis:
-        logging.info("Turning on IIS service")
-        logging.info("iisreset /start")
-        os.system("iisreset /start")
+    logging.info(f"Database {str(database.get_name)} weights {db_size} ({round(db_size / gb, 3)} Gb) " +
+                 f"(Free space is {free_space} ({round(free_space / gb, 3)} Gb))")
 
+    if db_size * 2.5 < free_space:
+        backup_filename = f"{database.get_name}_backup_{get_current_date()}"
 
-def stop_services(services, sch_tasks, iis):
-    logging.info("Stopping SQLiteStudio")
-    logging.info("Taskkill /IM SQLiteStudio.exe /F")
-    os.system("Taskkill /IM SQLiteStudio.exe /F")
+        logging.info(f"Copy {database.get_name} from {database.get_dir} to {database.get_backup_dir}")
 
-    for serv in services:
-        logging.info("Turning off \"" + serv + "\" service")
-        logging.info("sc stop \"" + serv + "\"")
-        os.system("sc stop \"" + serv + "\"")
+        if re.match(fr'^tcgi.*_archive$', str(database.get_name), re.IGNORECASE):
+            move_file(database.get_path, f'{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db')
 
-    for task in sch_tasks:
-        logging.info("Turning off \"" + task + "\" task")
-        logging.info("schtasks /end /tn \"" + task + "\"")
-        logging.info("schtasks /change /tn \" " + task + "\" /disable")
-        os.system("schtasks /end /tn \"" + task + "\"")
-        os.system("schtasks /change /tn \" " + task + "\" /disable")
+        if not Path(f'{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db').exists():
+            shutil.copy(database.get_path, f'{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db')
 
-    if iis:
-        logging.info("Turning off IIS service")
-        logging.info("iisreset /stop")
-        os.system("iisreset /stop")
+        if Path(f"{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db").exists():
+            logging.info(f"Connecting to database {database.get_name}")
+            con = sqlite3.connect(f"{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db")
+            cur = con.cursor()
+            logging.info(f"{database.get_name} backup started")
+            if Path(f'{database.get_backup_dir}\\{backup_filename}.db').exists():
+                os.rename(f'{database.get_backup_dir}\\{backup_filename}.db',
+                          f'{database.get_backup_dir}\\{backup_filename}_copy_' + f'{get_current_time()}'.replace(':',
+                                                                                                                  '_') + '.db')
+            cur.execute(f"VACUUM INTO '{database.get_backup_dir}\\{backup_filename}.db';")
 
+            logging.info(f"{database.get_name} backup ended")
+            con.commit()
+            con.close()
 
-def load_yaml(path: Path) -> dict[str, tp.Any]:
-    if path.exists():
-        logging.info("Config file reading started")
-        with Path(path).open("r") as f:
-            cfg_yaml = yaml.safe_load(f)
-        if not isinstance(cfg_yaml, dict):
-            logging.error(f"Config file has no top-level mapping: {path}")
-            raise TypeError(f"Config file has no top-level mapping: {path}")
-        return cfg_yaml
+            logging.info(f"Deleting of {database.get_name}_copy_for_backup")
+
+            os.remove(f"{database.get_backup_dir}\\{database.get_name}_copy_for_backup.db")
+
+            db_backup_size = os.path.getsize(f"{database.get_backup_dir}\\{backup_filename}.db")
+            logging.info(f"Backup {backup_filename}.db weights {db_backup_size} ({round(db_backup_size / gb, 3)} Gb)")
+            if db_backup_size / db_size < 0.5:
+                logging.error(f"{database.get_name} backup is too small!")
+            else:
+                logging.info(f"{database.get_name} database is backed up SUCCESSFULLY!")
+        else:
+            logging.error(f"Copy is missing!")
+
     else:
-        logging.error("Config file does not exist!")
-        exit()
+        logging.error("NOT enough free space for backup!")
 
-
-date_str = datetime.datetime.now().strftime('%d.%m.%Y')
-log_file = f"backup_{date_str}.log"
-
-logging.basicConfig(level=logging.INFO, filename=log_file, filemode='a',
-                    format="%(asctime)s %(levelname)s: %(message)s")
-with open(log_file, 'a') as f:
-    f.write("\nNEW BACKUP STARTED \n")
 
 if __name__ == "__main__":
+    log_file = f"backup_{get_current_date()}.log"
+
+    logging.basicConfig(level=logging.INFO, filename=log_file, filemode='a',
+                        format="%(asctime)s.%(msecs)03d %(levelname)s: %(message)s",
+                        datefmt='%d.%m.%Y %H:%M:%S')
+
+    with open(log_file, 'a') as f:
+        f.write(f"\n")
+    logging.info("NEW BACKUP STARTED")
 
     config_yaml = load_yaml(Path("config.yml"))
     config = Config.model_validate(config_yaml)
@@ -92,14 +88,17 @@ if __name__ == "__main__":
     delete_files(Path.cwd(), config.get_delete_after_days, fr"^backup_.*.log$")
     logging.info("Old logs deleted")
 
-    for db in config.databases:
-        if db is None:
-            config.databases.remove(db)
-        else:
-            if db.get_delete_after_days is None:
-                db.set_delete_after_days(config.get_delete_after_days)
+
+
+    # deleting dublicates and bad db's from list
+    config.databases = [db for db in config.databases if db is not None]
 
     for db in config.databases:
-        db.create_backup()
+        if db.get_delete_after_days is None:
+            db.set_delete_after_days(config.get_delete_after_days)
+
     for db in config.databases:
-        db.archive_backup()
+        create_backup(db)
+
+    for db in config.databases:
+        archive_backup(db)
--- schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from .database import Database
-from .config import Config
-
-__all__ = [
-    "Database",
-    "Config",
-]
